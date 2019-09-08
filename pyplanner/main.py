81bc0c1590eafeb1af5582efcf0415abae198a74
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-09-07 23:04:59 -0600

    Restructure project

---
+++ pyplanner/main.py
@@ -0,0 +1,6 @@
+import sys
+from PySide2.QtWidgets import QApplication
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    sys.exit(app.exec_())
---
+++ pyplanner/widgets/taskwidget.py
@@ -0,0 +1,5 @@
+from PySide2 import QtCore, QtWidgets, QtGui
+
+class TaskWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__(parent, f)
